c1ba8bca8caa57d801af44c579baeb275b85d662
Shawn <shawn.nigel@gmail.com>
2017-01-09 16:43:45 +0530

    basic stuff

---
+++ BookDataStore.rb
@@ -0,0 +1,15 @@
+module BookDataStore
+
+  def get_author_name id
+    return "Shawn"
+  end
+
+  def get_publisher_name id
+    return "Wiley #{id}"
+  end
+
+  def get_isbn id
+    return "ASUFDSSFDA#{id}"
+  end
+
+end
---
+++ book.rb
@@ -0,0 +1,19 @@
+
+class BookShop
+  def initialize book_id, book_data
+    @id=book_id
+    @book_data=book_data
+  end 
+  
+  def author
+   authorname= @book_data.get_author_name(@id)
+   return "Author name is "+authorname
+  end
+  
+  def publisher
+   publishername=@book_data.get_publisher_name(@id)
+   return "Publisher name is"+publishername
+  end
+
+
+end
